ad72c0c582eb6db1dfbc15e3113d7db40c9c7f2e
unknown <wamuyuwanjohi97@gmail.com>
2020-06-17 05:23:56 +0300

    Adding student View

--- student/views.py
+++ student/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from student.models import Student
 
 # Create your views here.
+
+def index(request):
+	data = Student.objects.all()
+	return HttpResponse(data)
--- studentproject/urls.py
+++ studentproject/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings 
 from django.conf.urls.static import static
-
+from student.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('' , index , name='')
 ]+ static(settings.MEDIA_URL , document_root =settings.MEDIA_ROOT)
