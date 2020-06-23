d2ca20b84db97ba5549f272e030264ee83026a12
unknown <wamuyuwanjohi97@gmail.com>
2020-06-23 16:41:20 +0300

    Adding on the template pages

--- src/movie/views.py
+++ src/movie/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView , DetailView
+from .models import Movie
+class MovieList(ListView):
+	model = Movie
+
+class MovieDetail(DetailView):
+	model = Movie
 
 # Create your views here.
